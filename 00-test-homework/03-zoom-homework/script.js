66c72c2a98d229bf75e0ab1c2f827e7776facb3b
vasyl_artus <basa94389@gmail.com>
2021-10-03 17:30:13 +0300

    done

--- 00-test-homework/03-zoom-homework/script.js
+++ 00-test-homework/03-zoom-homework/script.js
@@ -8,11 +8,9 @@ const getMaxDigit = (number) => {
     }
   };
   
-  console.log(getMaxDigit(9123569));
-
   //2. Ступінь числа
 
-  const pow = (x,y) =>{
+  const getPow = (x,y) =>{
   let result = 1;
 
   for(let i = 0; i < y ; i++) {
@@ -20,41 +18,67 @@ const getMaxDigit = (number) => {
   }
   return result;
   };
-  console.log(pow(6,2));
+ 
 
-  const pow2 = (x1, n2)=> {
+  const getPow2 = (x1, n2)=> {
     if (n2 === 1) {
       return x1;
     } else {
-      return x1 * pow2(x1, n2 - 1);
+      return x1 * getPow2(x1, n2 - 1);
     }
   }
-  console.log(pow2(6,2));
-  
+
   //3. Форматування імені
   
-  const changeletter = (name = 'невідомо') =>{ 
+    const changeletter = (name = 'невідомо') =>{ 
     
     return(name[0]).toUpperCase() + name.toLowerCase().slice(1)};
  
-    console.log(changeletter ());
+  //4. Вирахування залишку 
 
-      //4. Вирахування залишку 
-
-      const remainder = (salary, tax1, tax2) =>{
-       let tax1Given = (parseFloat(tax1))*0.01; 
-       let tax2Given = (parseFloat(tax2))*0.01;
-       return salary - salary*(tax1Given + tax2Given);
+       const remainder = (salary, tax1, tax2) =>{
+       let tax1Given = (parseFloat(tax1)) * 0.01; 
+       let tax2Given = (parseFloat(tax2)) * 0.01;
+       return salary - salary * (tax1Given + tax2Given);
     }
-    console.log(remainder (1000,"12.3%","2.3%" ));
 
      //5. Рандомне ціле число від N до M 
 
-     function randomInteger(n, m) {
+     const getrandomInteger = (n, m) => {
         
         let rand = n + Math.random() * (m + 1 - n);
         return Math.floor(rand);
      }
-     console.log(randomInteger (1,10));
+     
+     //6. Підрахунок однакової букви в слові 
+
+        const countLetter = (letter, word) => {
+         let stringleter = (letter.toString()).toLowerCase();
+         
+         let stringWord = (word.toString()).toLowerCase();
+
+         let sum = 0
+        
+         for (let char of stringWord) {
+            if (char === stringleter)
+            sum += 1;
+        }
+
+        return sum;
+        
+        };
+
+console.log(`Функція №1: ${getMaxDigit(1236)}`);
+console.log(`Функція №2(1): ${getPow(2, 6)}`);
+console.log(`Функція №2(2): ${getPow(2, 6)}`);
+console.log(`Функція №3: ${changeletter('вЛАД')}`);
+console.log(`Функція №4: ${remainder(1000,"12.5%","5.6%")}`);
+console.log(`Функція №5: ${getrandomInteger(1, 10)}`);
+console.log(`Функція №6: ${countLetter('а', 'Асталавіста')}`);
+         
+
+
+
+
 
 
