41351def7fae2d184d517ecf7def2d381f29dd71
vasyl_artus <basa94389@gmail.com>
2021-10-05 21:54:43 +0300

    done

--- 00-test-homework/04-zoom-homework/script.js
+++ 00-test-homework/04-zoom-homework/script.js
@@ -1,3 +1,4 @@
+// 1 Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. У вас повинен вийти вкладений масив з парами студентів: [["Олександр", "Олена"], [..], [...]];
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 const marks = [4, 5, 5, 3, 4, 5];
@@ -17,13 +18,13 @@ const getPairs = (massive) => {
 
     const pairs = [];
     for (let i = 0; i < boys.length; i++) {
-        pairs.push(((boys[i].split(" ")).concat((girls[i]).split(" "))));
+        pairs.push(((boys[i].split(" ")).concat(girls[i].split(" "))));
     };
     return pairs
-   /* for (let i = 0; i < boys.length; i++) {
-        pairs.push(boys[i].concat(girls[i]));
-    };
-    return pairs*/
+    /* for (let i = 0; i < boys.length; i++) {
+         pairs.push(boys[i].concat(girls[i]));
+     };
+     return pairs*/
 
     /* for (let i = 0; i < boys.length; i++) {
         pairs.push((boys[i].concat(girls[i]).split(" ")));
@@ -34,21 +35,33 @@ const getPairs = (massive) => {
 };
 console.log(getPairs(students));
 
+// 2 Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена", "Теорія автоматів"], [...], [...]]
 const getThemes = (themes) => {
     const studentPairs = [];
     for (let i = 0; i < themes.length; i++) {
-       
+
         studentPairs.push(getPairs(students)[i].join(" і ").split(","));
     }
     const studentsThemes = [];
 
-   for (let i = 0; i < themes.length; i++) {
+    for (let i = 0; i < themes.length; i++) {
 
-        (studentsThemes.push((((studentPairs[i]).concat((themes[i]).split())))));
+        studentsThemes.push((studentPairs[i]).concat(themes[i].split()));
     };
     return studentsThemes
-}
-
-
+};
 
 console.log(getThemes(themes));
+
+// 3 Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
+
+const getPoint = (students, girls) => {
+    const studentMarks = [];
+    for (let i = 0; i < girls.length; i++) {
+        studentMarks.push(((students[i].split(" ")).concat(girls[i].toString().split(" "))));
+
+    };
+    return studentMarks
+}
+
+console.log(getPoint(students, marks));
