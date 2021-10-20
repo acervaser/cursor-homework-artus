2b834706084aab5fc45ad2fbfc77ed65d4f0532a
vasyl_artus <basa94389@gmail.com>
2021-10-20 12:12:01 +0300

    виправив згідно зауважень

--- 00-test-homework/00-testhomework.js
+++ 00-test-homework/00-testhomework.js
@@ -1,2 +1,23 @@
 console.log("Test homework");
 console.log("I change my homework");
+// 4 творіть функцію getMySalary(country) – яка буде писати в консоль об'єкт виду: { salary: number, taxes: number, profit: number } кожні 10 секунд.
+const getMySalary = function (country) {
+
+    const getRandomSalary = (minSalary = 1500, maxSalary = 2000) => Math.floor(minSalary + Math.random()
+        * (maxSalary + 1 - minSalary));
+    const taxes =  +(getRandomSalary() * this.tax).toFixed(2);
+
+    const profit = +(getRandomSalary() - taxes).toFixed(2);
+
+        return Object.assign({ salary: getRandomSalary(), taxes, profit })
+};
+
+console.log("Функція #1");
+console.log(getMyTaxes.call(ukraine, 1000));
+console.log("Функція #2");
+console.log(getMiddleTaxes.call(ukraine));
+console.log("Функція #3");
+console.log(getTotalTaxes.call(ukraine));
+console.log("Функція #4");
+const result = setInterval(() => console.log( getMySalary.call(ukraine)), 10000 );
+setTimeout(() => { clearInterval(result) }, 100000);
--- 00-test-homework/08-zoom-homework/script.js
+++ 00-test-homework/08-zoom-homework/script.js
@@ -22,14 +22,16 @@ const getTotalTaxes = function (country) {
 
 // 4 творіть функцію getMySalary(country) – яка буде писати в консоль об'єкт виду: { salary: number, taxes: number, profit: number } кожні 10 секунд.
 const getMySalary = function (country) {
-
+       
     const getRandomSalary = (minSalary = 1500, maxSalary = 2000) => Math.floor(minSalary + Math.random()
         * (maxSalary + 1 - minSalary));
     const taxes =  +(getRandomSalary() * this.tax).toFixed(2);
 
     const profit = +(getRandomSalary() - taxes).toFixed(2);
     
-        return Object.assign({ salary: getRandomSalary(), taxes: taxes, profit: profit })
+    setInterval(() =>console.log( Object.assign({ salary: getRandomSalary(), taxes, profit })),10000)
+    return "Please wait"
+        
 };
 
 console.log("Функція #1");
@@ -39,5 +41,5 @@ console.log(getMiddleTaxes.call(ukraine));
 console.log("Функція #3");
 console.log(getTotalTaxes.call(ukraine));
 console.log("Функція #4");
-const result = setInterval(() => console.log( getMySalary.call(ukraine)), 10000 );
-setTimeout(() => { clearInterval(result) }, 100000);
+console.log( getMySalary.call(ukraine))
+
