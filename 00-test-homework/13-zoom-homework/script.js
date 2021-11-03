354e5dd8049c394d42b43e5785014ff7238a15cb
vasyl_artus <basa94389@gmail.com>
2021-11-03 21:57:59 +0200

    maybe done 2

--- 00-test-homework/13-zoom-homework/script.js
+++ 00-test-homework/13-zoom-homework/script.js
@@ -1,35 +1,15 @@
 
-/*Створіть функцію, яка повертає проміс getRandomChinese(length). Функція працює таким чином:
-
-Запускається цикл(підказка: можна використовувати while) length раз, кожен прохід циклу дивимось на останні 5 цифр результату виклику методу Date.now() Наприклад отримали const sign = 16086;.
-Конвертуємо отриману цифу в рядок String.fromCharCode(sign); та чекаємо 50 ms
-Функція gerRandomChinese(length) повинна повернути рядок довжиною (length) з китайськими ієрогліфами. Час роботи проміса має складати length * 50ms.
-(Якщо викликати getRandomChinese(4), отримаємо результат "촛궻簽紙" за 200ms*/
-
- /*function getRandomChinese(length) {
-    let string = "";
-     let i = 0
- while (i < length) { 
- let p = setInterval(() => {
-
-        let sing = String.fromCharCode(Date.now().toString().slice(-5)) 
-     string += sing
-         }, 2000);
-         i++   
-}
-  return string  
-}
-console.log(getRandomChinese(4))*/
 function getRandomChinese(length) {
 let string = ""
 const time = setInterval(() => {
-    
-  let sing = String.fromCharCode(Date.now().toString().slice(-5))
+  const sing = String.fromCharCode(Date.now().toString().slice(-5))
   string += sing 
   if(string.length === length){
     console.log(string)
+    clearInterval(time)
   }
-   }, 2000);
+   }, 50);
+   return "Homework#13"
 }
 console.log(getRandomChinese(4))
    
