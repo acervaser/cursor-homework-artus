547ca8b9cbc6f9f49e5ae31955810f8bb5d043a3
vasyl_artus <basa94389@gmail.com>
2021-11-09 12:07:31 +0200

    d

--- 00-test-homework/14-zoom-homework/script.js
+++ 00-test-homework/14-zoom-homework/script.js
@@ -2,53 +2,102 @@ const baseUrl = "https://swapi.dev/api";
 const listOfCharacters = document.getElementById("list-of-characters");
 const getCharacterBtn = document.getElementById("character");
 const input = document.getElementById('input');
+const listOfPlanets = document.getElementById("list-of-planets")
+const getPlanetsBtn = document.getElementById("planet");
+const getnextBtn = document.getElementById("next");
+const getprevBtn = document.getElementById("prev");
+let currentPage = 1;
+const translateIntoWookiee = document.getElementById("wookiee");
+const translateIntoEnglish = document.getElementById("english");
+const formatWookie = ""
+
+
 
 const getPeople = () => {
-    if(input.value >= 1 && input.value <= 6 ){
+    if (input.value >= 1 && input.value <= 6) {
         listOfCharacters.innerHTML = "in the galaxy far far away...";
-    }else{
+    } else {
         listOfCharacters.innerHTML = "The failures is the best teacher our ...";
         return
     }
     axios
-    .get(`${baseUrl}/films/${input.value}/`)
-    .then((response) => {
-        let list = "";
-        let listElems = response.data.characters;
-        listElems.map(item => {
-            axios
-            .get(item)
-            .then((response) => {
-                console.log(response)
-                let r =   Object.values(response)
-                console.log(r)
-                Object.values(response).map((status) => {
-                        list += `
-                        <div >  
-                            <h3>Full name: ${status}</h3>
-                            <p>Date of birth: ${status}</p>
-                            <p>Gender: ${status}</p>
-                        </div>
-                        `
-                    
-                });
-                listOfCharacters.innerHTML += list;
+        .get(`${baseUrl}/films/${input.value}/${formatWookie}`)
+        .then((response) => {
+            const listElems = response.data.characters;
+            listOfCharacters.innerHTML = ''
+            listElems.map((person) => {
+                getInfo(person)
             })
-            .catch((err) => {
-                console.log("Error:", err);
-                listOfCharacters.innerHTML = "Error occured :" + err.message;
-            });
+                .catch((err) => {
+                    console.log("Error:", err);
+                    listOfCharacters.innerHTML = "Error occured :" + err.message;
+                });
         });
-    })
-    .catch((err) => {
-        console.log("Error:", err);
-        listOfCharacters.innerHTML = "Error occured :" + err.message;
-    });
+}
+
+
+
+function getInfo(person) {
+    axios
+        .get(person)
+        .then((response) => {
+            const info = `
+                    <div class="character">
+                    <h3>${response.data.name}</h3>
+                    <p>${response.data.birth_year}</p>
+                    <p>${response.data.gender}</p>
+                    </div>`
+            listOfCharacters.innerHTML += info
+        })
+        .catch((err) => {
+            console.log("Error:", err);
+            listOfCharacters.innerHTML = "Error occured :" + err.message;
+        });
 };
 
+const getPlanets = () => {
+    axios
+        .get(`${baseUrl}/planets/?page=${currentPage}`)
+        .then((response) => {
+            const listElems = response.data.results;
+            console.log(listElems)
+            listOfPlanets.innerHTML = ""
+            listElems.map((planet) => {
+                const planetInfo = `
+<div class="character">
+<h3>${planet.name}</h3>
+<p>${planet.orbital_period}</p>
+<p>${planet.population}</p>
+</div>`
+                listOfPlanets.innerHTML += planetInfo
+            })
+
+
+        })
+        .catch((err) => {
+            console.log("Error:", err);
+            listOfCharacters.innerHTML = "Error occured :" + err.message;
+        });
+
+}
+
 getCharacterBtn.addEventListener('click', getPeople);
-  //console.log(charakterList)  `
-                    //<div class="character">
-                   // <h3>${response2.data.name}</h3>
-                    //</div>`
-                    //console.log(charakterList)
+getPlanetsBtn.addEventListener('click', getPlanets);
+
+function getnextPage() {
+    if (currentPage === 6) return;
+    console.log(currentPage)
+    currentPage++
+    getPlanets()
+
+}
+function getprevPage() {
+    if (currentPage === 1) return;
+    console.log(currentPage)
+    currentPage--
+    getPlanets()
+
+};
+
+getprevBtn.addEventListener('click', getprevPage);
+getnextBtn.addEventListener('click', getnextPage);
