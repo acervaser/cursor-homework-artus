3a2e4a954dcab58c29e403194e293f99c031ba3c
vasyl_artus <basa94389@gmail.com>
2021-11-09 12:00:52 +0200

    midlle

--- 00-test-homework/14-zoom-homework/script.js
+++ 00-test-homework/14-zoom-homework/script.js
@@ -1,34 +1,54 @@
-const baseUrl = "https://swapi.dev/api/";
-const list = document.getElementById("list");
-const btn = document.getElementById("load");
+const baseUrl = "https://swapi.dev/api";
+const listOfCharacters = document.getElementById("list-of-characters");
+const getCharacterBtn = document.getElementById("character");
+const input = document.getElementById('input');
 
-const getPeoples = () => {
-    list.innerHTML = "in the galaxy far far away...";
-    //const charakterList = new Array;
+const getPeople = () => {
+    if(input.value >= 1 && input.value <= 6 ){
+        listOfCharacters.innerHTML = "in the galaxy far far away...";
+    }else{
+        listOfCharacters.innerHTML = "The failures is the best teacher our ...";
+        return
+    }
     axios
-        .get(`${baseUrl}films/2`)
-        .then((response) => {
-            console.log(response)
-
-            const listElems = response.data.characters.map((item) => {
-                axios
-                    .get(item)
-                    .then((response) => {
-                        console.log(response)  `
-                    <div class="character">
-                    <h3>${response.data.name}</h3>
-                    </div>`
-
-                    })
-                    .catch((err) => {
-                        console.log("Error:", err);
-                    });
-                console.log(listElems);
-                list.innerHTML = listElems;
-            })
-                .catch((err) => {
-                    console.log("Error:", err);
+    .get(`${baseUrl}/films/${input.value}/`)
+    .then((response) => {
+        let list = "";
+        let listElems = response.data.characters;
+        listElems.map(item => {
+            axios
+            .get(item)
+            .then((response) => {
+                console.log(response)
+                let r =   Object.values(response)
+                console.log(r)
+                Object.values(response).map((status) => {
+                        list += `
+                        <div >  
+                            <h3>Full name: ${status}</h3>
+                            <p>Date of birth: ${status}</p>
+                            <p>Gender: ${status}</p>
+                        </div>
+                        `
+                    
                 });
+                listOfCharacters.innerHTML += list;
+            })
+            .catch((err) => {
+                console.log("Error:", err);
+                listOfCharacters.innerHTML = "Error occured :" + err.message;
+            });
         });
-}
-btn.onclick = getPeoples
+    })
+    .catch((err) => {
+        console.log("Error:", err);
+        listOfCharacters.innerHTML = "Error occured :" + err.message;
+    });
+};
+
+getCharacterBtn.addEventListener('click', getPeople);
+  //console.log(charakterList)  `
+                    //<div class="character">
+                   // <h3>${response2.data.name}</h3>
+                    //</div>`
+                    //console.log(charakterList)
