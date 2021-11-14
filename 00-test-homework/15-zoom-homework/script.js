75091469e95027dccb787d76dddf8724d4b35438
vasyl_artus <basa94389@gmail.com>
2021-11-14 20:12:56 +0200

    start

--- 00-test-homework/15-zoom-homework/script.js
+++ 00-test-homework/15-zoom-homework/script.js
@@ -3,13 +3,12 @@ function* createIdGenerator () {
     while(i > 0) {
         yield i;
         i++
-    }
+    };
 
-}
+};
 const idGenerator = createIdGenerator();
 console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
-
 
 
 function* newFontGenerator(fontSize) {
@@ -22,10 +21,11 @@ function* newFontGenerator(fontSize) {
             fontSize -= 2;
             result = yield fontSize
         }else result = yield fontSize;
-    }
-}
+    };
+};
 const fontGenerator = newFontGenerator(14);
 console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("up").value);
 console.log(fontGenerator.next("up").value);
 console.log(fontGenerator.next("up").value);
 console.log(fontGenerator.next().value);
---
+++ 00-test-homework/16-zoom-homework/src/index.js
@@ -0,0 +1,8 @@
+const testVar = 100;
+const anotherTestVar = 100;
+console.log(testVar + anotherTestVar);
+console.log("test");
+
+if(testVar){
+    console.log("Дивишся?"); 
+}
