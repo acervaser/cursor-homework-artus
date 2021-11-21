b97050a75d110731a4ca0e96c250db0fa2c2f023
vasyl_artus <basa94389@gmail.com>
2021-11-21 21:22:12 +0200

    add FullPost

---
+++ 18-zoom-homework/src/components/FullPost.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+
+const userData = {
+    name: "Anakin skywalker",
+    photo: ANAKIN_IMAGE,
+    nickname: "@dart_vader"
+};
+
+const postData = {
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image: { RAY_IMAGE }
+}
+
--- 18-zoom-homework/src/components/UserInfo.jsx
+++ 18-zoom-homework/src/components/UserInfo.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+
+
 const UserInfo = () => {
 
-}
+};
+export default UserInfo
