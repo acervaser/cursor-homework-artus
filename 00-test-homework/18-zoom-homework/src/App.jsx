0f0478d731b881c4c32b0750c5158d3bdad17f83
vasyl_artus <basa94389@gmail.com>
2021-11-22 14:50:42 +0200

    i dont now

--- 00-test-homework/18-zoom-homework/src/App.jsx
+++ 00-test-homework/18-zoom-homework/src/App.jsx
@@ -1,27 +1,27 @@
 
- import './App.css';
+import './App.css';
 import React from "react";
 import UserInfo from "./components/UserInfo";
 import PostStatistics from "./components/PostStatistikcs";
 import Baby from "./assests/img/baby_yoda.jpg";
 import Yoda from "./assests/img/Yoda.jpg";
 
-const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+const BABY_IMAGE = Baby;
 
-const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+const YODA_IMAGE = Yoda;
 
 
 
 
 const userData = {
     name: "Baby Yoda",
-    photo: Baby,
+    photo: BABY_IMAGE,
     nickname: "@baby_yoda"
 };
 
 const postData = {
     content: "My dear father, I miss you so much...",
-    image: Yoda,
+    image: YODA_IMAGE,
     date: "26 feb"
 }
 
@@ -30,12 +30,12 @@ const statisticsData = {
     shares: 146,
     likes: 887,
 }
-const App = ({ content, image, date }) => {
+const App = () => {
     return (
         <div className="post">
             <div className="wrapper">
-                <UserInfo name={userData.name} image={ANAKIN_IMAGE} nickname={userData.nickname} />
-                <span className="post-date">{postData.date}</span>
+                <UserInfo name={userData.name} image={userData.photo} nickname={userData.nickname} />
+                <span className="post-date">{postData.image}</span>
                 <span className="post-content">{postData.content}</span>
             </div>
 
--- 00-test-homework/18-zoom-homework/src/components/UserInfo.jsx
+++ 00-test-homework/18-zoom-homework/src/components/UserInfo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const UserInfo = ({ name, image,nickname }) => {
+const UserInfo = ({ name, image, nickname}) => {
     return(
-<div className="user-info">
-    <img className="user-image" srs={image} alt="user's photo" />
+<>
+    <img className="user-image" srs={image} alt="user" />
     <div className="user-name">
       {name}  
     </div>
     <a className="user-nickname" href="#">{nickname}</a>
-</div>
+</>
     );
 };
 export default UserInfo
