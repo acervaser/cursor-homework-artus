e1fc487d47f15ae3167e098eb7492005162557c0
vasyl_artus <basa94389@gmail.com>
2021-11-22 10:07:27 +0200

    UserInfo done

--- 18-zoom-homework/src/components/UserInfo.jsx
+++ 18-zoom-homework/src/components/UserInfo.jsx
@@ -3,9 +3,14 @@ import React from "react";
 
 
 const UserInfo = ({ name, image,nickname }) => {
+    return(
 <div className="user_info">
-    <img className="user_image" srs="image" alt="user's photo" />
-    
+    <img className="user_image" srs={image} alt="user's photo" />
+    <div className="user_name">
+      {name}  
+    </div>
+    <a className="user_nickname" href="#">{nickname}</a>
 </div>
+    );
 };
 export default UserInfo
